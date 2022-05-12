647436688a89faf214ab4613a8bbd5e9e76863ca
beans3 <beans3.k@gmail.com>
2022-05-12 23:41:20 +0900

    first commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+    </head>
+    <body>
+        <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+            Test
+        </div>   
+    </body>
+</html>
